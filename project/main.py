56df402456cb66ee243c8539bf636d1ef21c6bca
Paul92 <paul.gafton@gmail.com>
2020-05-18 12:26:23 +0200

    Finished digit extraction

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 import argparse
 from utils import *
+from video import *
 from Net import Net
 import torch 
 from torch import Tensor 
@@ -7,56 +8,126 @@ from torch import nn
 from torch.nn import functional as F
 from torch import optim
 
-
-data_base_path = os.path.join(os.pardir, 'data')
-data_folder = 'lab-03-data'
-data_part2_folder = os.path.join(data_base_path, data_folder, 'part2')
+import os
 
 
-parser = argparse.ArgumentParser(description='Project 1 - Classification.')
+def process_video(input_path):
+    # Read the video from specified path
+    cam = cv2.VideoCapture(input_path)
 
-parser.add_argument('--input',
-                    type = str, default = "./data/robot_parcours_1.avi",
-                    help = 'Path to input file')
+    currentframe = 0
+    cv2.namedWindow("video")
+    cv2.namedWindow("bbs")
 
-parser.add_argument('--output',
-                    type = str, default = None,
-                    help = 'Path to output file')
+    while(True):
+        # reading from source
+        ret, frame = cam.read()
 
-parser.add_argument('--training',
-                    action = 'store_true', default = False,
-                    help = 'Train the model for digits recognition on the MNIST dataset')
+        if ret:
+            normalized = normalize(frame)
 
-parser.add_argument('--display_training',
-                    action = 'store_true', default = False,
-                    help = 'If training, print performances')
+            thresholdedArrow = thresholdArrow(normalized).astype('uint8') * 255
 
-parser.add_argument('--model_digits',
-                    type = str, default = 'model/model_MNIST.pt',
-                    help = 'Path to trained model for digits')
+            arrowBbox = bounding_box(thresholdedArrow)
+            arrowBbox = expandBbox(arrowBbox, 20)
 
-parser.add_argument('--epochs',
-                    type = int, default = 25,
-                    help = 'Number of epochs for the training (default:25)')
+            cv2.rectangle(frame, arrowBbox[0:2], arrowBbox[2:4], (0, 0, 255))
 
-parser.add_argument('--mnist_data',
-                    type = str, default = data_part2_folder,
-                    help = 'Path to the MNIST data')
+            thresholded = threshold(normalized).astype('uint8') * 255
+            bbs = [expandBbox(bb, 2) for bb in bounding_boxes(thresholded) if not isOverlapping(bb, arrowBbox)]
 
-args = parser.parse_args()
+            patches = extractPatches(thresholded, bbs)
+            patches = normalizePatches(patches)
 
-## create images from the video &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-create_data(args.input)
+          #  printPatches(patches)
 
-## generate and train model for digits recognition &&&&&&&&&&&&&&&&&&&&&&&&&&
-if args.training: 
-    #train the parameters of the model on the dataset
-    train_model(args.model_digits, args.epochs, args.display_training, args.mnist_data)
 
-if os.path.exists(args.model_digits):
-    model=Net()
-    model.load_state_dict(torch.load(args.model_digits))
-    model.eval()
-    print('hello you made it here')
-    #use output=model(input) to use the model 
+            # TODO: Classify patches
+
+
+
+            cv2.imshow("video", thresholded)
+            draw_bbs(frame, bbs)
+
+            cv2.imshow("bbs", frame)
+
+            cv2.waitKey()
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+
+
+if __name__ == '__main__':
+
+    data_base_path = os.path.join(os.pardir, 'data')
+    data_folder = 'lab-03-data'
+    data_part2_folder = os.path.join(data_base_path, data_folder, 'part2')
+
+
+    parser = argparse.ArgumentParser(description='Project 1 - Classification.')
+
+    parser.add_argument('--input',
+                        type = str, default = "./data/robot_parcours_1.avi",
+                        help = 'Path to input file')
+
+    parser.add_argument('--output',
+                        type = str, default = None,
+                        help = 'Path to output file')
+
+    parser.add_argument('--training',
+                        action = 'store_true', default = False,
+                        help = 'Train the model for digits recognition on the MNIST dataset')
+
+    parser.add_argument('--eval',
+                        action = 'store_true', default = False,
+                        help = 'Evaluate the model')
+
+    parser.add_argument('--run',
+                        action = 'store_true', default = True,
+                        help = 'Run from video')
+
+    parser.add_argument('--display_training',
+                        action = 'store_true', default = False,
+                        help = 'If training, print performances')
+
+    parser.add_argument('--model_digits',
+                        type = str, default = 'model/model_MNIST.pt',
+                        help = 'Path to trained model for digits')
+
+    parser.add_argument('--epochs',
+                        type = int, default = 25,
+                        help = 'Number of epochs for the training (default:25)')
+
+    parser.add_argument('--mnist_data',
+                        type = str, default = data_part2_folder,
+                        help = 'Path to the MNIST data')
+
+    args = parser.parse_args()
+
+    ## create images from the video &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    ## generate and train model for digits recognition &&&&&&&&&&&&&&&&&&&&&&&&&&
+    if args.training: 
+        #train the parameters of the model on the dataset
+        train_model(args.model_digits, args.epochs, args.display_training, args.mnist_data)
+
+    if args.eval:
+        if os.path.exists(args.model_digits):
+            model=Net()
+            model.load_state_dict(torch.load(args.model_digits))
+            model.eval()
+            print('hello you made it here')
+            #use output=model(input) to use the model 
+
+    if args.run:
+        process_video(args.input)
 
---
+++ project/video.py
@@ -0,0 +1,167 @@
+import os
+
+import cv2
+import numpy as np
+import skimage.morphology
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+
+def normalize(frame):
+    fact = 255/frame[70:100, 40:100].mean()
+    normalized_im = np.clip(fact*frame, 0, 255).astype('uint8')
+    return normalized_im
+
+def threshold(frame):
+    return frame.mean(axis=2) < 175
+
+def thresholdArrow(frame):
+    r = frame[:,:,2]
+    g = frame[:,:,1]
+    b = frame[:,:,0]
+
+    return np.logical_and(np.logical_and(r > 100, b < 80), g < 80)
+
+
+def bounding_box(frame):
+    rows = np.any(frame, axis=1)
+    cols = np.any(frame, axis=0)
+    rmin, rmax = np.where(rows)[0][[0, -1]]
+    cmin, cmax = np.where(cols)[0][[0, -1]]
+    return cmin, rmin, cmax, rmax
+
+
+def merge_two_bboxes(A, B):
+    x11, y11, x21, y21 = A
+    x12, y12, x22, y22 = B
+    return (min(x11, x12), min(y11, y12), max(x21, x22), max(y21, y22))
+
+
+def merge_bboxes(bboxes, to_merge):
+    for merge in to_merge:
+        A = bboxes[merge[0]]
+        B = bboxes[merge[1]]
+        bboxes[merge[0]] = merge_two_bboxes(A, B)
+        bboxes[merge[1]] = merge_two_bboxes(A, B)
+    return bboxes
+
+
+def bounding_boxes(frame):
+    labels, nb_labels = skimage.morphology.label(frame, return_num=True)
+
+    frame_size = frame.shape[0] * frame.shape[1]
+
+    bounding_boxes = []
+
+    for i in range(nb_labels):
+        area = np.sum(labels == i) / frame_size
+        if area > 1e-3 or area < 1e-5:
+            labels[labels == i] = 0
+        else:
+            bounding_boxes.append(bounding_box(labels==i))
+
+    img = (labels != 0).astype('uint8') * 255
+
+    bb_centroids = []
+    for bb in bounding_boxes:
+        bb_centroids.append(((bb[0] + bb[2]) // 2, (bb[1] + bb[3]) // 2))
+
+    bb_centroids = np.array(bb_centroids)
+    dist_matrix = distance.cdist(bb_centroids, bb_centroids)
+    closest = np.where(dist_matrix < 20)
+
+    to_merge = []
+
+    for i in range(closest[0].shape[0]):
+        source = closest[0][i]
+        target = closest[1][i]
+        if source != target and source < target:
+            to_merge.append((source, target))
+
+    bboxes = merge_bboxes(bounding_boxes, to_merge)
+
+    filtered_bb = []
+    for bb in bounding_boxes:
+        h = bb[3] - bb[1]
+        w = bb[2] - bb[0]
+        aspect_ratio = w / h
+        if aspect_ratio > 0.1:
+            filtered_bb.append(bb)
+        else:
+            img[bb[1]-5:bb[3]+5, bb[0]-5:bb[2]+5] = 0
+            labels[bb[1]-5:bb[3]+5, bb[0]-5:bb[2]+5] = 0
+
+    return set(filtered_bb)
+
+
+def draw_bbs(img, bbs):
+    if len(img.shape) == 3 and img.shape[2] == 3:
+        color = (0, 255, 0)
+    else:
+        color = 150
+
+    for bb in bbs:
+        cv2.rectangle(img, bb[0:2], bb[2:4], color)
+
+def expandBbox(bbox, amount):
+    return bbox[0] - amount, bbox[1] - amount, bbox[2] + amount, bbox[3] + amount
+
+
+def isOverlapping(A, B):
+    x1min, y1min, x1max, y1max = A
+    x2min, y2min, x2max, y2max = B
+    return x1min <= x2max and x2min <= x1max and y1min <= y2max and y2min <= y1max
+
+def _resize_pad_to_square_keep_aspect_ratio(img, size, pad_color=0):
+    h, w = img.shape[:2]
+    sh, sw = size
+
+    if h > sh or w > sw:
+        interp = cv2.INTER_AREA
+    else:
+        interp = cv2.INTER_CUBIC
+
+    aspect = w / h
+    if aspect > 1:
+        new_w = sw
+        new_h = np.round(new_w / aspect).astype(int)
+        pad_vert = (sh - new_h) / 2
+        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1:
+        new_h = sh
+        new_w = np.round(new_h * aspect).astype(int)
+        pad_horz = (sw - new_w) / 2
+        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else:
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    if len(img.shape) is 3 and not isinstance(pad_color, (list, tuple, np.ndarray)):
+        pad_color = [pad_color] * 3
+
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(
+        scaled_img, pad_top, pad_bot, pad_left, pad_right,
+        borderType=cv2.BORDER_CONSTANT, value=pad_color
+    )
+
+    return scaled_img, new_w, new_h, pad_left, pad_top
+
+def extractPatches(img, bboxes):
+    patches = [img[bbox[1]:bbox[3], bbox[0]:bbox[2]] for bbox in bboxes]
+    return patches
+
+def normalizePatch(patch):
+    return _resize_pad_to_square_keep_aspect_ratio(patch, (28, 28))[0]
+
+def normalizePatches(patches):
+    return [normalizePatch(patch) for patch in patches]
+
+def printPatches(patches):
+    f, ax = plt.subplots(1, len(patches))
+    for i in range(len(patches)):
+        print(patches[i].shape)
+        ax[i].imshow(patches[i], cmap='gray')
+    plt.show()
+
